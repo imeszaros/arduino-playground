3eb1d70a802be4bff5036e3b2e058d71c53dff5a
Istvan Meszaros <contact.imeszaros@gmail.com>
2017-12-03 16:37:20 +0100

    Added button to start/stop music.

--- project.ino
+++ project.ino
@@ -1,15 +1,42 @@
+#include <Bounce2.h>
 #include "pmf_player.h"
 #include "pmf_tune.cpp"
 
+#define BTN_MUSIC 8
+
 static pmf_player s_player;
+Bounce debouncer = Bounce();
+
+bool music = true;
 
 void setup()
 {
-  s_player.start(s_pmf_file);
-  delay(1000);
+	//Serial.begin(9600);
+  pinMode(BTN_MUSIC, INPUT);
+
+  debouncer.attach(BTN_MUSIC);
+  debouncer.interval(5);
+
+  if (music) {
+	  s_player.start(s_pmf_file);
+  }
 }
 
 void loop()
 {
-  s_player.update();
+	debouncer.update();
+
+	if (music) {
+		s_player.update();
+	}
+
+  if (debouncer.rose()) {
+	 // Serial.println(music);
+	  music = !music;
+	  if (music) {
+		  s_player.start(s_pmf_file);
+	  } else {
+		  s_player.stop();
+	  }
+  }
 }
